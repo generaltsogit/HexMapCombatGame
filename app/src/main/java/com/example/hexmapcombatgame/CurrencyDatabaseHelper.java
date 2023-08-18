40de50b8b5ec0fe64baa017b374849acf654ac68
willd <willd4000@gmail.com>
2023-08-18 17:53:37 -0500

    added currency, database not updating.

---
+++ app/src/main/java/com/example/hexmapcombatgame/CurrencyDatabaseHelper.java
@@ -0,0 +1,72 @@
+package com.example.hexmapcombatgame;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CurrencyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "game_currency.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table and column names
+    private static final String TABLE_CURRENCY = "currency";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_AMOUNT = "amount";
+
+    // Create table SQL statement
+    private static final String CREATE_TABLE_CURRENCY = "CREATE TABLE " + TABLE_CURRENCY + " (" +
+            COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_AMOUNT + " INTEGER)";
+
+    public CurrencyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_CURRENCY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CURRENCY);
+        onCreate(db);
+    }
+
+    // CRUD operations
+
+    public long insertCurrency(int amount) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_AMOUNT, amount);
+        return db.insert(TABLE_CURRENCY, null, values);
+    }
+
+    @SuppressLint("Range")
+    public int getCurrencyAmount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {COLUMN_AMOUNT};
+        Cursor cursor = db.query(TABLE_CURRENCY, projection, null, null, null, null, null);
+        int amount = 0;
+        if (cursor.moveToFirst()) {
+            amount = cursor.getInt(cursor.getColumnIndex(COLUMN_AMOUNT));
+        }
+        cursor.close();
+        return amount;
+    }
+
+    public int updateCurrencyAmount(int newAmount) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_AMOUNT, newAmount);
+        return db.update(TABLE_CURRENCY, values, null, null);
+    }
+
+    // Add more methods for CRUD operations as needed
+}
+
+
--- app/src/main/java/com/example/hexmapcombatgame/ProfilePageActivity.java
+++ app/src/main/java/com/example/hexmapcombatgame/ProfilePageActivity.java
@@ -1,13 +1,18 @@
 package com.example.hexmapcombatgame;
 
 import android.content.Intent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.view.View;
 
 public class ProfilePageActivity extends AppCompatActivity {
+
+    private CurrencyDatabaseHelper dbHelper;
+    private TextView currencyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,38 @@ public class ProfilePageActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_profile_page);
+
+        dbHelper = new CurrencyDatabaseHelper(this);
+        currencyTextView = findViewById(R.id.currencyTextView);
+
+        updateCurrencyDisplay();
     }
 
     public void goBackToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void addCurrency(View view) {
+        int currentCurrency = dbHelper.getCurrencyAmount();
+        int newCurrency = currentCurrency + 100;
+        dbHelper.updateCurrencyAmount(newCurrency);
+
+        updateCurrencyDisplay();
+
+        Log.d("Currency", "Currency added: " + newCurrency); // debug, tells terminal that currency is added
+    }
+
+    private void updateCurrencyDisplay() {
+        int currentCurrency = dbHelper.getCurrencyAmount();
+        currencyTextView.setText("Currency: " + currentCurrency);
+        System.out.println(currentCurrency);
+        Log.d("Currency", "Currency updated: "); // debug, tells terminal that currency is updated
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dbHelper.close();
     }
 }
